7a458bb0a77a1b303b1f2a75f10b5b799f06013f
alyssacowen <alyssarcowen@gmail.com>
2023-02-04 15:50:23 -0500

    Created data members Created default constructor Created parameterized constructor Created setters Created getters In NewClass, created object of type Course Set values for the variables using setters Made print statements with getters to run code and test

--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -14,4 +14,11 @@ public class NewClass {
         c1.setCode("CSC 229");
         c1.setID(23260);
         c1.setName("Moaath Alrajab");
+        //here I made print statements to run my code to see if it works
+         System.out.println("Course Code: " + c1.getCode());
+         System.out.println("Course ID/Reference Number: " + c1.getID());
+         System.out.println("Professor Name: " + c1.getName());
+    }
+    
+}
 
