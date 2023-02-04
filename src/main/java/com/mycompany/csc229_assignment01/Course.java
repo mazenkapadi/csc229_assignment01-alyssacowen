fb4a13a0c36852cad59761f2c94c5bb7b42b48bb
alyssacowen <123402425+alyssacowen@users.noreply.github.com>
2023-02-04 14:33:17 -0500

    Update Course.java

--- src/main/java/com/mycompany/csc229_assignment01/Course.java
+++ src/main/java/com/mycompany/csc229_assignment01/Course.java
@@ -33,5 +33,42 @@ package com.mycompany.csc229_assignment01;
  * @author MoaathAlrajab
  */
 public class Course {
-
+//here I will create the data members
+    private int ID;
+    private String Name;
+    private String Code;
+    //here I will create the default constructor
+    public Course(){
+        ID = 0;
+        Name = " ";
+        Code = " ";
+    }
+    //here I will create the parameterized constructor
+    public Course(int ID, String Name, String Code){
+        ID = ID;
+        Name = Name;
+        Code = Code;
+    }
+    //here I will create the setters
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    public void setCode(String Code) {
+        this.Code = Code;
+    }
+        //here I will create the getters
+    public int getID() {
+        return ID;
+    }
+    public String getName() {
+        return Name;
+    }
+    public String getCode() {
+        return Code;
+    }
 }
+
+
