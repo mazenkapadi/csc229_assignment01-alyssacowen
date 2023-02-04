82556bd63ca9b0c351ad738eef4313a4d18738ce
alyssacowen <123402425+alyssacowen@users.noreply.github.com>
2023-02-04 14:35:49 -0500

    created new object of type Course, set values for the variables, print with getters

--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -8,7 +8,16 @@ package com.mycompany.csc229_assignment01;
 public class NewClass {
     
      public static void main(String[] args) {
-        System.out.println("Hello World!");
+         //here I am creating a new object of type Course
+        Course c1 = new Course();
+        //here I am setting the values of variables using setters
+        c1.setCode("CSC 229");
+        c1.setID(23260);
+        c1.setName("Moaath Alrajab");
+//here I made print statements to run my code to see if it works
+         System.out.println("Course Code: " + c1.getCode());
+         System.out.println("Course ID/Reference Number: " + c1.getID());
+         System.out.println("Professor Name: " + c1.getName());
+         //ran to test
     }
-    
 }
