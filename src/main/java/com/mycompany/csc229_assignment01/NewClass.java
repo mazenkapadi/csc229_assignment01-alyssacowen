414d2f03bdf684c6ff4e3f5ec69e946bebcbdb98
alyssacowen <alyssarcowen@gmail.com>
2023-02-04 15:47:13 -0500

    Created data members Created default constructor Created parameterized constructor Created setters Created getters In NewClass, created object of type Course

--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -8,16 +8,6 @@ package com.mycompany.csc229_assignment01;
 public class NewClass {
     
      public static void main(String[] args) {
-         //here I am creating a new object of type Course
+        //here I am creating a new object of type Course
         Course c1 = new Course();
-        //here I am setting the values of variables using setters
-        c1.setCode("CSC 229");
-        c1.setID(23260);
-        c1.setName("Moaath Alrajab");
-//here I made print statements to run my code to see if it works
-         System.out.println("Course Code: " + c1.getCode());
-         System.out.println("Course ID/Reference Number: " + c1.getID());
-         System.out.println("Professor Name: " + c1.getName());
-         //ran to test
-    }
-}
+
